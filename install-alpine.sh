fea93809327d20fc7a52afb415c8acedce0c2efa
tutacat <fhwbml@gmail.com>
2024-11-27 16:39:47 +0000

    Set TCG to be multi-threaded

--- install-alpine.sh
+++ install-alpine.sh
@@ -1,16 +1,15 @@
-pkg update -y
-pkg upgrade -y
-pkg install wget -y
-pkg install qemu-system-x86-64-headless qemu-utils -y
-cd $HOME
-mkdir alpine-linux
-cd alpine-linux/
-wget https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-virt-3.14.0-x86_64.iso
+echo "This script uses some default memory values, you should tailor them to your system and use-case."
+pkg upgrade -y curl qemu-system-x86-64-headless qemu-utils
+BASE='~/alpine-linux'
+eval set BASEDIR="$BASE"
+[ ! -d "$BASEDIR" ] && mkdir -p "$BASEDIR" || (echo 'Failed to create $BASE.'; exit 1)
+curl -LCZo alpine-virt-x86_64.iso https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-virt-3.14.0-x86_64.iso
 qemu-img create -f qcow2 alpine.qcow2 15G
-qemu-system-x86_64 -smp 2 -m 2048 \
+qemu-system-x86_64 -smp 1,cores=2 -m 2048 \
+  -accel tcg,thread=multi
   -drive file=alpine.qcow2,if=virtio \
   -netdev user,id=n1,hostfwd=tcp::2222-:22 \
   -device virtio-net,netdev=n1 \
-  -cdrom alpine-virt-3.14.0-x86_64.iso -boot d \
+  -cdrom alpine-virt-x86_64.iso -boot d \
   -nographic
 
