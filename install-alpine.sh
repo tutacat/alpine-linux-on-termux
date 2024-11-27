e19c3e99c64a869cb88a05be6025243636e89d6d
tutacat <fhwbml@gmail.com>
2024-11-27 16:42:12 +0000

    Bump Alpine to 3.18

--- install-alpine.sh
+++ install-alpine.sh
@@ -3,7 +3,7 @@ pkg upgrade -y curl qemu-system-x86-64-headless qemu-utils
 BASE='~/alpine-linux'
 eval set BASEDIR="$BASE"
 [ ! -d "$BASEDIR" ] && mkdir -p "$BASEDIR" || (echo 'Failed to create $BASE.'; exit 1)
-curl -LCZo --compression alpine-virt-x86_64.iso https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-virt-3.14.0-x86_64.iso
+curl -LCZo --compression alpine-virt-x86_64.iso https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-virt-3.18.0-x86_64.iso
 qemu-img create -f qcow2 alpine.qcow2 15G
 qemu-system-x86_64 -smp 1,cores=2 -m 2048 \
   -accel tcg,thread=multi
